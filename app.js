9a4ac41952f6faea81d7ab6530adfb33066cfce1
Matthew Marion <matthewjmarion29@gmail.com>
2017-12-25 21:16:16 -0600

    Working | first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+require('dotenv').load();
+const twit = require('twit');
+const snoowrap = require('snoowrap');
+const snoostorm = require('snoostorm');
+const config = require('./config.js');
+
+const twitter = new twit(config);
+
+const reddit = new snoowrap({
+	userAgent: process.env.USER_AGENT,
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	username: process.env.REDDIT_USER,
+	password: process.env.REDDIT_PASS
+});
+
+const client = new snoostorm(reddit);
+
+const redditStream = client.SubmissionStream({
+	subreddit: process.env.SUBREDDIT,
+	results: 1
+});
+
+redditStream.on('submission', function(post) {
+	console.log('New submission by ' + post.author.name + " - " + post.url);
+	postTweet(post.author.name, post.title, post.url);
+});
+
+let postTweet = function(author, title, url) {
+	let tweetText = title + " by " + author + '. Read more: ' + url;
+	twitter.post('statuses/update',
+	{
+		status: tweetText,
+		function(err, data, response) {
+			if (err) {
+				console.log("Error : " + err);
+			}
+		}
+	})
+}
